04479ac603e2a3c55e5caf1825973af7bce47e5b
Andrii Suboch <andrii.suboch@gmail.com>
2020-08-31 12:07:50 +0300

    Updates

---
+++ precache-manifest.554cf1b08b6ddfa3c667a1f462f53869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a058606ebf1d8c1173d91c2de694b60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "91214508d0d37b1b95de",
+    "url": "./static/css/main.64c4a65d.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91214508d0d37b1b95de",
+    "url": "./static/js/main.460ae29c.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
